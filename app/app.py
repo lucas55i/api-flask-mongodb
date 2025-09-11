69f609840e4b90c6d0df71a06335997a93bc439f
Lucas Jesus <lucas.devj@outlook.com>
2025-09-11 20:00:26 -0300

    feat: format lint

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
+"""Flask application to manage MongoDB data."""
+import os
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId
-import os
 
 app = Flask(__name__)
 
@@ -11,49 +12,62 @@ MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_PORT = os.getenv("MONGO_PORT")
 MONGO_DB   = os.getenv("MONGO_DB")
 
-MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource=admin"
+MONGO_URI = (
+    f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/"
+    f"{MONGO_DB}?authSource=admin"
+)
+
 print("Conected em:", MONGO_URI)
 
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
 colecao = db["users"]
 
+
+"""Module init"""
 def serialize_doc(doc):
+    """Module doc"""
     return {**doc, "_id": str(doc["_id"])}
 
 @app.route("/")
 def home():
+    """Module Init app API Flask + MongoDB UP """
     return jsonify({"message": "API Flask + MongoDB UP"})
 
 @app.route("/users", methods=["POST"])
-def criar_usuario():
+def create_user():
+    """Create user"""
     dados = request.get_json()
     resultado = colecao.insert_one(dados)
     return jsonify({"id": str(resultado.inserted_id)}), 201
 
 @app.route("/users", methods=["GET"])
-def listar_users():
+def list_users():
+    """List users"""
     users = list(colecao.find())
     return jsonify([serialize_doc(u) for u in users])
 
-@app.route("/users/<id>", methods=["GET"])
-def buscar_usuario(id):
-    usuario = colecao.find_one({"_id": ObjectId(id)})
-    if usuario:
-        return jsonify(serialize_doc(usuario))
+@app.route("/users/<user_id>", methods=["GET"])
+def list_user_by_id(user_id):
+    """Get user by {user_id}"""
+    user = colecao.find_one({"_id": ObjectId(user_id)})
+    if user:
+        return jsonify(serialize_doc(user))
     return jsonify({"error": "User not found"}), 404
 
-@app.route("/users/<id>", methods=["PUT"])
-def atualizar_usuario(id):
+@app.route("/users/<user_id>", methods=["PUT"])
+def update_user_by_id(user_id):
+    """Update user by {user_id}"""
     dados = request.get_json()
-    resultado = colecao.update_one({"_id": ObjectId(id)}, {"$set": dados})
+    resultado = colecao.update_one({"_id": ObjectId(user_id)}, {"$set": dados})
     if resultado.matched_count > 0:
         return jsonify({"message": "User updated successfully"})
     return jsonify({"error": "User not found"}), 404
 
-@app.route("/users/<id>", methods=["DELETE"])
-def deletar_usuario(id):
-    resultado = colecao.delete_one({"_id": ObjectId(id)})
+@app.route("/users/<user_id>", methods=["DELETE"])
+def delete_user_by_id(user_id):
+    """Delete user by {user_id}"""
+    resultado = colecao.delete_one({"_id": ObjectId(user_id)})
     if resultado.deleted_count > 0:
         return jsonify({"message": "User deleted successfully"})
     return jsonify({"error": "User not found"}), 404
